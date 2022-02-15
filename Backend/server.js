c324530ed76f48610702139f378905310db7f9f2
rakshith <rakshit.s.gowda@gmail.com>
2022-02-15 12:54:39 +0530

    connection to mongodb and code refrac

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,34 @@
 import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import connectDB from './db/connectDB.js'
 
+dotenv.config()
 const app = express()
-const PORT = process.env.PORT || 8888
 
-app.listen(PORT, () => {
-  console.log('hi ')
-})
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || process.env.API_PORT
+
+// --------------------------------------------------------------------------------
+// initailaize app with db connect
+// --------------------------------------------------------------------------------
+const start = async () => {
+  try {
+    await connectDB(
+      process.env.MONGO_URI.replace(
+        '<password>',
+        process.env.MONGO_URI_PASSWORD
+      )
+    )
+    app.listen(PORT, () => {
+      console.log(`Hi from port ${PORT}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
